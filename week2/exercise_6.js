35801b493c6b49d331fa882652585dc0b473dec3
Lowrance Rawung <LowRainSir1509@Pradhita.localdomain>
2018-09-24 16:58:51 +0700

    revised

--- week2/exercise_6.js
+++ week2/exercise_6.js
@@ -1,4 +1,4 @@
-/*
+
 //1. Melakukan Looping Menggunakan While
 // LOOPING PERTAMA
 var loop1 = 2;
@@ -6,26 +6,22 @@ while(loop1 <21){
   console.log(loop1 + ' - I love coding');
   loop1 = loop1+2;
 }
-
 //LOOPING KEDUA
 var loop2 = 20
 while(loop2 > 0){
   console.log(loop2 + ' - I will become fullstack developer');
   loop2 = loop2 - 2;
 }
-
-
 //2. Melakukan Looping Menggunakan For
 //Looping PERTAMA
 for(var i=1; i < 21; i++){
   console.log(i + " - I love coding");
 }
-
 //Looping KEDUA
 for(var a=20; a > 0; a--){
   console.log(a + " - I will become fullstack developer");
 }
-*/
+
 //3. Angka Ganjil dan Genap
 for(var b = 0; b <= 100; b++){
   if(b%2 === 0){
